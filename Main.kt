3bd92ac1814e5ef0b77921562b93f8ec9bced0db
Sadnik13 <Sadnik13@users.noreply.github.com>
2025-09-29 22:58:35 +0000

    docs: Update documentation

--- Main.kt
+++ Main.kt
@@ -2,3 +2,6 @@ fun fib(n:Int):Long = if (n<2) n.toLong() else fib(n-1)+fib(n-2)
 fun main(){
   println("fib(20)="+fib(20))
 }
+
+// docs: Update documentation [31ec2dee]
+// Updated: 2025-09-29 22:58:35 UTC
